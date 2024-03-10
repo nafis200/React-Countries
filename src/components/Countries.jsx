6c42f34586949acdbec815eec644abbcbc403049
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-10 21:31:57 +0600

    fifth commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "./country/Country";
-
+import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);    
     useEffect(()=>{
@@ -11,11 +11,13 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-            {
+           <div className="country-container">
+           {
                countries.map(country =>
                 <Country key={country.cca3} country = {country}></Country>
                 )   
             }
+           </div>
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,11 +1,24 @@
 
+import { useState } from 'react'
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country
+    const {name, flags,population,area,cca3} = country
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = ()=>{
+        setVisited(!visited)
+    }
+    
     return (
         <div className='country'> 
             <h3>name:{name.common} </h3>
             <img src={flags.png} alt="" />
+            <p>Populations: {population}</p>
+            <p>area: {area}</p>
+            <p><small>code: {cca3}</small></p>
+            <button onClick={handleVisited}>visited</button>
+            {visited && 'i have visited'}
+
         </div>
     )
 }
