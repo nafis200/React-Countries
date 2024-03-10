564e543435fd4af2ea0103a2e8ebde3f4ab70456
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-10 21:00:01 +0600

    third commit

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,11 @@
 
-
+import './Country.css'
 const Country = ({country}) => {
-    const {name} = country
+    const {name, flags} = country
     return (
-        <div>
+        <div className='country'> 
             <h3>name:{name.common} </h3>
+            <img src={flags.png} alt="" />
         </div>
     )
 }
